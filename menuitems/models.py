e57336757827710c63941aba66b664938ed2a217
isaacbarcroft <i_barcroft@yahoo.com>
2021-10-28 18:17:02 -0400

    pillow

--- menuitems/models.py
+++ menuitems/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Menuitem(models.Model):
         title = models.CharField(max_length = 255, default='SOME STRING')
         category = models.CharField(max_length = 255, default='SOME STRING')
-        image = models.ImageField(upload_to='profiles/', null=True)
+        image = models.ImageField( null=True)
         price = models.IntegerField(null=True)
         desc =models.TextField(default='descr')
 
